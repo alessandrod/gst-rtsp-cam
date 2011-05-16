37fc8dbd47aadb60f071fd9c791245edbbb984f0
Alessandro Decina <alessandro.d@gmail.com>
2011-05-16 23:19:07 +0200

    tune x264enc for zerolatency

--- src/gst-rtsp-cam-media-factory.c
+++ src/gst-rtsp-cam-media-factory.c
@@ -76,7 +76,7 @@ G_DEFINE_TYPE (GstRTSPCamMediaFactory, gst_rtsp_cam_media_factory, GST_TYPE_RTSP
 
 static CodecDescriptor codecs[] = {
   { "theora", "theoraenc ! rtptheorapay name=pay%d pt=96" },
-  { "h264", "x264enc ! rtph264pay name=pay%d pt=96" },
+  { "h264", "x264enc tune=zerolatency ! rtph264pay name=pay%d pt=96" },
   { "vp8", "vp8enc quality=10 ! rtpvp8pay name=pay%d pt=96" },
   { "vorbis", "vorbisenc ! rtpvorbispay name=pay%d pt=97" },
   { "amrnb", "amrnbenc ! rtpamrpay name=pay%d pt=97" },
